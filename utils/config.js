81ddd9bce8484392056809ea9e1b6e170a759579
Evgeny Proskurnikov <vesper182@inbox.ru>
2021-01-18 17:28:00 +0300

    d-1. fix hosts

--- utils/config.js
+++ utils/config.js
@@ -3,15 +3,15 @@ const DEV_DB_HOST = 'mongodb://localhost:27017/newsdb';
 const SALT_ROUNDS = 10;
 const HOSTS = [
   'http://localhost:3000',
-  'http://www.api.news-explorer.evpro.students.nomoreparties.xyz',
-  'https://www.api.news-explorer.evpro.students.nomoreparties.xyz',
-  'http://api.news-explorer.evpro.students.nomoreparties.xyz',
-  'https://api.news-explorer.evpro.students.nomoreparties.xyz',
-  'http://www.news-explorer.evpro.students.nomoreparties.xyz',
-  'https://www.news-explorer.evpro.students.nomoreparties.xyz',
-  'http://news-explorer.evpro.students.nomoreparties.xyz',
-  'https://news-explorer.evpro.students.nomoreparties.xyz',
-]
+  'http://www.api.nexplorer.evpro.students.nomoreparties.xyz',
+  'https://www.api.nexplorer.evpro.students.nomoreparties.xyz',
+  'http://api.nexplorer.evpro.students.nomoreparties.xyz',
+  'https://api.nexplorer.evpro.students.nomoreparties.xyz',
+  'http://www.nexplorer.evpro.students.nomoreparties.xyz',
+  'https://www.nexplorer.evpro.students.nomoreparties.xyz',
+  'http://nexplorer.evpro.students.nomoreparties.xyz',
+  'https://nexplorer.evpro.students.nomoreparties.xyz',
+];
 
 module.exports = {
   DEV_SECRET,
